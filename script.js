23fd17e0f2198c02039ef21d20db16a0d983f6a8
Megdoud-DEV-PHP <117215344+Megdoud-DEV-PHP@users.noreply.github.com>
2022-11-21 23:56:16 +0100

    modif

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+
 document.getElementById("connexion").addEventListener("submit", function(e) {
 e.preventDefault();
+
 var erreur;
 var login = document.getElementById("login");
-var login = document.getElementById("password");
-if (!login.value) {
-    erreur = "Veuillez renseigner votre login";
-}
+var password = document.getElementById("password");
+
+
 
 if (!password.value) {
     erreur = "Veuillez renseigner votre mode pass";
 }
+
+if (!login.value) {
+    erreur = "Veuillez renseigner votre login";
+}
+
 if (erreur) {
- e.preventDefault();
- document.getElementById("erreur").innerHTML = erreur;
- return false;
+   e.preventDefault();
+   document.getElementById("erreur").innerHTML = erreur;
+   return false;
 } else {
     alert("Formulaire envoyé !");
-}
+      }
 
 
 
