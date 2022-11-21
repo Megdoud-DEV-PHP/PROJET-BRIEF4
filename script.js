fb7ece50781e766109c9b0820cdaa34711a2e303
Megdoud-DEV-PHP <117215344+Megdoud-DEV-PHP@users.noreply.github.com>
2022-11-21 23:23:40 +0100

    modi

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.getElementById("connexion").addEventListener("submit", function(e) {
+e.preventDefault();
+var erreur;
+var login = document.getElementById("login");
+var login = document.getElementById("password");
+if (!login.value) {
+    erreur = "Veuillez renseigner votre login";
+}
 
+if (!password.value) {
+    erreur = "Veuillez renseigner votre mode pass";
+}
+if (erreur) {
+ e.preventDefault();
+ document.getElementById("erreur").innerHTML = erreur;
+ return false;
+} else {
+    alert("Formulaire envoyé !");
+}
+
+
+
+})
