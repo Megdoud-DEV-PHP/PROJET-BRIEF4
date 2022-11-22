e65833ee7c7737fcdec1ba5c57236deb43b67058
Megdoud-DEV-PHP <117215344+Megdoud-DEV-PHP@users.noreply.github.com>
2022-11-22 13:54:14 +0100

    hhh

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ var password = document.getElementById("password");
 
 
 
+//Deuxième cas on suppose que le premier champ login n'est pas correcte.
+
 if (password.value != "admin") {
-    erreur = "Veuillez renseigner votre mode pass";
+    erreur = "Veuillez renseigner votre mode passe";
 }
+
+//Deuxième cas on suppose que le premier champ login n'est pas correcte.
 
 if (login.value != "admin") {
     erreur = "Veuillez renseigner votre login";
 }
+
+// Premier cas on suppose que tous les champs ne sont pas remplis.
+
+if ((!password.value) && (!login.value)) {
+    erreur = "Veuillez renseigner tous les champs";
+}
+
+
 
  if (erreur) {
         e.preventDefault();
