631662f2ab887f0c585d8d9754cb510c96701fab
Megdoud-DEV-PHP <117215344+Megdoud-DEV-PHP@users.noreply.github.com>
2022-11-22 16:10:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
+
 /* Application du java-script pour la page connexion */
 
 document.getElementById("connexion").addEventListener("submit", function(e) {
+
 var erreur;
 var login = document.getElementById("login");
 var password = document.getElementById("password");
-
 
 
 //Deuxième cas on suppose que le premier champ login n'est pas correcte.
@@ -19,9 +21,9 @@ if (login.value != "admin") {
     erreur = "Veuillez renseigner votre login";
 }
 
-// Premier cas on suppose que tous les champs ne sont pas remplis.
+ // Premier cas on suppose que tous les champs ne sont pas remplis.
 
-if ((!password.value) && (!login.value)) {
+ if ((!password.value) && (!login.value)) {
     erreur = "Veuillez renseigner tous les champs";
 }
 
@@ -34,60 +36,13 @@ if ((!password.value) && (!login.value)) {
      } else {
          alert("Formulaire envoyé !");
            }
+
+
 })
 
 
 
-    // deuxième façon de réaliser la validation du formulaire en javascript avec la boucle for en considérant l'élement html input comme un une variable d'un tableau.
-    /*
-    document.getElementById("connexion").addEventListener("submit", function(e) {
-    var erreur;
-    var inputs = document.getElementById("connexion").getElementsByTagName("input"); 
-    console.log(inputs);
-    for ( var i = 0; i < inputs.length; i++) {
-       
-        
-        if(!inputs["password"].value != "admin") {
-            erreur = "Votre mode de passe est incorrecte !";
-        }
-
-        if(!inputs["login"].value != "admin") {
-            erreur = "Votre login est incorrecte !";
-        }
-
-        if(!inputs[i].value) {
-            erreur = "Veuillez renseigner tous les champs";
-        }
-
-    }
-
-    if (erreur) {
-        e.preventDefault();
-        document.getElementById("erreur").innerHTML = erreur;
-        return false;
-     } else {
-         alert("Formulaire envoyé !");
-           }
-})
-
-*/
-
-/* Application du java-script pour la page Exercice-1.html */
-
-    document.getElementById("execution").addEventListener("submit", function() {
-    var vide;
-    var information = document.getElementById("information");
-    var information1 = document.getElementById("information1");
-    var information2 = document.getElementById("information2");
-   
-   
-    var messages = information.value + information1.value + information2.value;
-    console.log(messages);
     
-    
-
-})
-
 
 
 
