8a30b38124a93ce8e62b692c133674d11b38cace
Megdoud-DEV-PHP <117215344+Megdoud-DEV-PHP@users.noreply.github.com>
2022-11-28 23:59:49 +0100

    kkk

--- JS/animation.js
+++ JS/animation.js
@@ -1,1 +1,12 @@
 
+
+const questionContainer = document.querySelector(".click-event");
+console.log(questionContainer);
+const btn1 = document.querySelector("#btn-1");
+const response = document.querySelector('h4');
+
+btn1.addEventListener(//click event btn-1
+'click', () => {
+response.classList.add("show-response");
+    
+});
